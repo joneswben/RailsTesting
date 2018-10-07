985d5b1d5b76080b65c275918471fa4df715fa9b
Ben Jones <joneswben@gmail.com>
2018-10-07 22:26:30 +0000

    Added Bootstrapper and Faker gem to create fake names.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
+  before_action :common_content, :only => [:index, :team, :about]
+  
+  
+  def common_content
+    @superhero_name = Faker::Superhero.name
+    @heropower = Faker::Superhero.power
+  end
   def index
+    
   end
   
   def team
   end
-  
+  def about
+  end
 end
